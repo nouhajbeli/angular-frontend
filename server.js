4f597048d6fb4099e9fac5a48524db3c2a405991
nouhajbeli <70015527+nouhajbeli@users.noreply.github.com>
2021-01-09 01:35:57 -0800

    update

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
-//Install express server
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/front'));
+app.use(express.static("./dist/front/index.html"));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angular-front/'}),
+app.get("/*", (req, res) =>
+  res.sendFile("index.html", { root: "dist/front/" })
 );
 
 // Start the app by listening on the default Heroku port
